99302d5865cfa2f70e39a64d121828d6821f4ecb
andrescri11 <USRDEL@USR16.local>
2017-05-30 16:58:22 -0500

    binding

--- 04-Angular/a-web/src/app/Componentes/inicio/inicio.component.ts
+++ 04-Angular/a-web/src/app/Componentes/inicio/inicio.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  nombre: string = "Daniel";
+  //cmmand + a y luego command + alt + l
+  arregloUsuario = [{
+      nombre: "Daniel",
+      apellido: "Freire",
+      conectado: true
+    },
+    {
+      nombre: "Mashi",
+      apellido: "Correa",
+      conectado: true
+    },
+    {
+      nombre: "Abdala",
+      apellido: "Bucaram",
+      conectado: false
+    }];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  cambiarNombre(): void {
+    this.nombre = "Rafico a Lenin";
+  }
+
+  cambiarOtroNombre(): void {
+    this.nombre = "Lenin a Rafico";
+  }
+
+  cambiarNombreInput(nombreEtiqueta) {
+    console.log(nombreEtiqueta.value);
+    console.log(nombreEtiqueta.type);
+    console.log(nombreEtiqueta.placeholder);
+    this.nombre = nombreEtiqueta.value;
+  }
 }
