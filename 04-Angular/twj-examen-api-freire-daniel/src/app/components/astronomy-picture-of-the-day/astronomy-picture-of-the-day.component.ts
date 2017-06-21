b77bc38e9509f55fec0ef2504f21fd238d64bed8
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-06-21 17:51:41 -0500

    cosas agregadas al examen

--- 04-Angular/twj-examen-api-freire-daniel/src/app/components/astronomy-picture-of-the-day/astronomy-picture-of-the-day.component.ts
+++ 04-Angular/twj-examen-api-freire-daniel/src/app/components/astronomy-picture-of-the-day/astronomy-picture-of-the-day.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AstronomyPictureOfTheDayClass} from '../../classes/AstronomyPictureOfTheDayClass';
-import {Http} from '@angular/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { AstronomyPictureOfTheDayClass } from '../../classes/AstronomyPictureOfTheDayClass';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-astronomy-picture-of-the-day',
@@ -11,37 +11,11 @@ export class AstronomyPictureOfTheDayComponent implements OnInit {
 
   @Input() newPicture: AstronomyPictureOfTheDayClass = new AstronomyPictureOfTheDayClass();
 
-  constructor(private _http: Http) { }
+  constructor(private _sanitizer: DomSanitizer) { }
+
+  url: SafeUrl;
 
   ngOnInit() {
-    this._http
-      .get('https://api.nasa.gov/planetary/apod?api_key=7n1yswIdVMbpWBH42bIJMPPyDu8EP3mhKlhd5k2y')
-      .subscribe(
-        (response) => {
-          console.log('Respuesta: ', response);
-        },
-        (error) => {
-          console.log('Error: ', error);
-        },
-        () => {
-          console.log('Finally');
-        }
-      );
-  }
-
-  consultarImagen(picture: AstronomyPictureOfTheDayClass) {
-    this._http
-      .get('https://api.nasa.gov/planetary/apod?api_key=7n1yswIdVMbpWBH42bIJMPPyDu8EP3mhKlhd5k2y&date=' + picture.date)
-      .subscribe(
-        (response) => {
-          this.newPicture = response.json();
-        },
-        (error) => {
-          console.log('Error: ', error);
-        },
-        () => {
-          console.log('Finally');
-        }
-      );
+    this.url = this._sanitizer.bypassSecurityTrustUrl(this.newPicture.url);
   }
 }
--- 04-Angular/twj-examen-api-freire-daniel/src/app/components/container/container.component.ts
+++ 04-Angular/twj-examen-api-freire-daniel/src/app/components/container/container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AstronomyPictureOfTheDayClass } from '../../classes/AstronomyPictureOfTheDayClass';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-container',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+  newPicture: AstronomyPictureOfTheDayClass = new AstronomyPictureOfTheDayClass();
+
+  checkboxFlag: boolean;
+
+  constructor(private _http: Http) { }
 
   ngOnInit() {
+    this.checkboxFlag = false;
+    console.log(this.checkboxFlag);
   }
 
+  consultarImagen(picture: AstronomyPictureOfTheDayClass) {
+    this._http
+      .get('https://api.nasa.gov/planetary/apod?api_key=7n1yswIdVMbpWBH42bIJMPPyDu8EP3mhKlhd5k2y&date=' + picture.date)
+      .subscribe(
+        (response) => {
+          this.newPicture = response.json();
+        },
+        (error) => {
+          console.log('Error: ', error);
+        },
+        () => {
+          console.log('Finally');
+        }
+      );
+  }
 }
