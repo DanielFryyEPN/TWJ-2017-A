e1ac84f055534693bb0fd70ba637f9b428d894c1
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-07-19 15:43:46 -0500

    token obtenido

--- 04-Angular/a-web/src/app/Componentes/login/login.component.ts
+++ 04-Angular/a-web/src/app/Componentes/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {TokenService} from '../../token.service';
+import { TokenService } from '../../token.service';
+import { NgForm } from '@angular/forms';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,29 @@ import {TokenService} from '../../token.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private _TokenService: TokenService) { }
+  constructor(private _TokenService: TokenService, private _http: Http) { }
 
   ngOnInit() {
-    console.log(this._TokenService.token);
   }
 
+  login(datos: NgForm) {
+    const correo = datos.value.correoV;
+    const pass = datos.value.passwordV;
+    console.log(correo);
+    console.log(pass);
+    this._http
+      .get('http://localhost:1337/Auth/logIn?correo=' + correo + '&password=' + pass)
+      .subscribe(
+        (res) => {
+          this._TokenService.token = res.text();
+          console.log('Token:', this._TokenService.token);
+        },
+        (err) => {
+          console.log('Error:', err);
+        },
+        () => {
+          console.log('Finally');
+        }
+      );
+  }
 }
--- 04-Angular/a-web/src/app/app.component.ts
+++ 04-Angular/a-web/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {TokenService} from './token.service';
+import { TokenService } from './token.service';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +8,26 @@ import {TokenService} from './token.service';
 })
 export class AppComponent {
   usuario: UsuarioInt = {
-    nombre: "",
-    apellido: "Freire"
+    nombre: '',
+    apellido: 'Freire'
   };
 
-  parrafo:string = "Hola";
-  background:string = "blue";
+  parrafo:string = 'Hola';
+  background:string = 'blue';
 
   constructor(private _TokenService: TokenService) {
     this._TokenService.token = 'Mi mama me mima';
-    this.usuario.nombre = "Daniel";
-    console.log("constructor");
+    this.usuario.nombre = 'Daniel';
+    console.log('constructor');
     this.holaMundo();
    console.log(this.saludar(this.usuario.nombre, this.usuario.apellido));
     setTimeout(() => {
-      this.usuario.nombre = "Enrique"
+      this.usuario.nombre = 'Enrique';
     }, 1000);
   }
 
   holaMundo() {
-    console.log("Hola mundo")
+    console.log('Hola mundo');
   }
 
   saludar(nombre: string, apellido?: string): string {
@@ -36,6 +36,6 @@ export class AppComponent {
 }
 
 interface UsuarioInt {
-  nombre: string,
-  apellido: string
+  nombre: string;
+  apellido: string;
 }
